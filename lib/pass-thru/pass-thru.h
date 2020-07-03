bc7457cf8ffb028800cf93039037eb67403d804a
Sam Tarakajian <sam@cycling74.com>
2020-07-03 15:28:21 -0400

    pass-thru unit test in place for pure joy

--- lib/pass-thru/pass-thru.h
+++ lib/pass-thru/pass-thru.h
@@ -3,8 +3,21 @@
 
 #include <stdint.h>
 
-void setup(void);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-uint16_t process(uint16_t input);
+typedef struct passthru_t {
+    uint16_t dummy;
+} passthru_t;
+
+void PT_init(passthru_t * pt);
+void PT_destroy(passthru_t *pt);
+
+void PT_process(passthru_t *pt, uint16_t *in, uint16_t *out, uint8_t channel_count);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* PASS_THRU_H */
--- test/test_desktop/test_pass_thru/test_pass_thru.cpp
+++ test/test_desktop/test_pass_thru/test_pass_thru.cpp
@@ -1,21 +1,41 @@
-#include <pass-thru.h>
+#include "test_pass_thru.hpp"
 #include <unity.h>
 
-// void setUp(void) {
-// // set stuff up here
-// }
+passthru_t pt;
 
-// void tearDown(void) {
-// // clean stuff up here
-// }
+void setUp(void) {
+// set stuff up here
+    PT_init(&pt);
+}
 
-void test_qua_test(void) {
-    TEST_ASSERT_EQUAL(32, 32);
+void tearDown(void) {
+// clean stuff up here
+    PT_destroy(&pt);
+}
+
+void test_pt(void) {
+    uint16_t in_data[4][2] = {
+        { 0, 1 },
+        { 0, 2 },
+        { 0, 3 },
+        { 0, 4 }
+    };
+    uint16_t out_data[4][2];
+
+    for (uint8_t s = 0; s < 4; s++) {
+        PT_process(&pt, in_data[s], out_data[s], 2);
+    }
+
+    for (uint8_t c = 0; c < 2; c++) {
+        for (uint8_t s = 0; s < 4; s++) {
+            TEST_ASSERT_EQUAL_UINT16(in_data[s][c], out_data[s][c]);
+        }
+    }
 }
 
 int main(int argc, char **argv) {
     UNITY_BEGIN();
-    RUN_TEST(test_qua_test);
+    RUN_TEST(test_pt);
     UNITY_END();
 
     return 0;
