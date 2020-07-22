d11037e18bcc6e337d10995a99e3765160c7624c
Sam Tarakajian <sam@cycling74.com>
2020-07-22 15:25:14 -0400

    moved operators to a separate repo

--- lib/cuteop/cuteop.h
+++
@@ -1,5 +0,0 @@
-#include "include/counter.h";
-#include "include/edge.h";
-#include "include/gate.h";
-#include "include/random.h";
-#include "include/thresh.h";
--- lib/cuteop/src/gate.c
+++
@@ -1,8 +0,0 @@
-#include "gate.h";
-
-void gate_init(t_gate *self) { /* no-op */}
-
-void gate_process(t_gate *self, uint16_t *in1, uint16_t *openOrClosed, uint16_t *out)
-{
-    *out = (*openOrClosed > 0) ? *in1 : 0;
-}
